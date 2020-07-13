f5897e37680e1fef842f5cdc9764e2328f64f58c
FahimKamal <fahimkamal63@gmail.com>
2020-07-13 21:01:07 +0600

    2nd commit Basic database model for tweet is created.

---
+++ tweetme-project/tweets/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.http import HttpResponse, Http404, JsonResponse
+from .models import Tweet
+
+
+# Create your views here.
+def home_view(request, *args, **kwargs):
+    return render(request, 'pages/home.html', context={}, status=200)
+
+
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    """
+    Rest API view
+    Consume by JavaScript or Swift or Java or IOS/Android
+    return json data
+    """
+    data = {
+        "id": tweet_id,
+    }
+    status = 200
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+        data["content"] = obj.content
+    except:
+        data['message'] = 'Not found'
+        status = 404
+        # raise Http404
+
+    return JsonResponse(data, status=status)
